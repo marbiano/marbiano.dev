0c5d6ab07a994872407e91df413db5270d3ea83c
Martin Bavio <mbavio@gmail.com>
2021-11-23 01:16:25 -0300

    Going live

---
+++ app/routes/about.tsx
@@ -0,0 +1,11 @@
+import type { MetaFunction, LoaderFunction } from "remix";
+import { useLoaderData, json, Link } from "remix";
+
+// https://remix.run/guides/routing#index-routes
+export default function Index() {
+  return (
+    <div className="remix__page">
+      <h1>About</h1>
+    </div>
+  );
+}
